ea55b08daf219576053b4c2ca9ef2da7b412e7ca
Rye-Catcher <35390703+Rye-Catcher@users.noreply.github.com>
2018-01-13 13:34:57 +0800

    Add files via upload

---
+++ Hello World C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	cout<<"Hello World"<<endl;
+	return 0;
+}
